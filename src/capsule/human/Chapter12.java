dd45d3aa1a1fc6ab67c64c5df9b405f60fa45406
user <user@localhost>
2023-03-20 08:53:56 +0900

    カプセル化 練習問題クリア

--- src/capsule/human/Chapter12.java
+++ src/capsule/human/Chapter12.java
@@ -6,7 +6,7 @@ public class Chapter12 {
 		System.out.println(man1.getName() + "は" + man1.getAge() + "才です");
 		man1.setAge(33);
 		System.out.println("誕生日を迎えたので" + man1.getAge() + "歳になりました");
-		man1.setName("珍子");
+		man1.setName("真田");
 		System.out.println("離婚したので、苗字が" + man1.getName() + "になりました");
 	}
 
