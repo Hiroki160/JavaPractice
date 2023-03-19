8492301d2a2acaa53ffe1125902898d781211885
user <user@localhost>
2023-03-19 11:20:28 +0900

    年齢を出力。

--- src/Practice01.java
+++ src/Practice01.java
@@ -2,8 +2,14 @@
 public class Practice01 {
 	public static void main(String[] main) {
 		String name;
+		int age;
+		
 		name = "Hiroki";
+		age = 26;
+		
 		System.out.println(name);
+		System.out.println(name + "は" + age + "歳です");
+
 	}
 
 }
