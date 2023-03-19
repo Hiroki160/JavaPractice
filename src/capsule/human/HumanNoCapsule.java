5b3c2a072d65a790b9b3924f3bedca5a1731d425
user <user@localhost>
2023-03-20 08:16:20 +0900

    カプセル化

--- src/capsule/human/HumanNoCapsule.java
+++ src/capsule/human/HumanNoCapsule.java
@@ -1,5 +1,12 @@
 package capsule.human;
 
 public class HumanNoCapsule {
+	public String name;
+	public int age; //インスタンスフィールド
+	
+	public HumanNoCapsule(String name, int age) {
+		this.name = name;
+		this.age = age; //コンストラクタ
+	}
 
 }
