c3d4d1fccfd2a2489a67275d7dd866652fbb58a3
user <user@localhost>
2023-03-22 14:55:05 +0900

    firstcommit

--- src/While01.java
+++ src/While01.java
@@ -3,7 +3,7 @@ public class While01 {
 	public static void main(String[] args) {
 		int number = 1;
 		while(number < 50) {
-			number *= 2;
+			number *= 1;
 			System.out.println(number);
 		}
 		
