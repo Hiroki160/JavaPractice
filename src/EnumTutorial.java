93f4ee7042723fb50430b9fa00d4fc377cebaed0
Hiroki160 <sahiyuno0934@gmail.com>
2023-04-04 07:15:24 +0900

    Enum

---
+++ src/EnumTutorial.java
@@ -0,0 +1,12 @@
+
+public class EnumTutorial {
+	public static void main(String[] args) {
+		DaysOfTheWeek days = DaysOfTheWeek.WEDNESDAY;
+		
+		if(days == DaysOfTheWeek.WEDNESDAY) {
+			System.out.println("Can't believe it's still Wednesday!!");
+		}
+	}
+
+
+}
---
+++ src/Practice03.java
@@ -0,0 +1,12 @@
+
+public class Practice03 {
+	public enum Dessert {
+		APPLE, ICE_CREAM, CAKE,
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(Dessert.APPLE);
+		System.out.println(Dessert.ICE_CREAM);
+		System.out.println(Dessert.CAKE);
+	}
+}
