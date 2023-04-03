f20c4285bfc99465270c7310b078c84e12944368
Hiroki160 <sahiyuno0934@gmail.com>
2023-04-04 07:26:22 +0900

    Enum

--- src/EnumTutorial.java
+++ src/EnumTutorial.java
@@ -1,10 +1,12 @@
 
 public class EnumTutorial {
 	public static void main(String[] args) {
-		DaysOfTheWeek days = DaysOfTheWeek.WEDNESDAY;
+		DaysOfTheWeek days = DaysOfTheWeek.THURSDAY;
 		
-		if(days == DaysOfTheWeek.WEDNESDAY) {
+		if(days == DaysOfTheWeek.FRIDAY) {
 			System.out.println("Can't believe it's still Wednesday!!");
+		} else {
+			System.out.println("Holly shit, I don't wanna work");
 		}
 	}
 
